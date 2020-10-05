be2c820a304ca8cfa5cb8fe49c1b51658b272028
Ishmael King <ishmaelrking@gmail.com>
2020-10-05 14:12:35 -0700

    Initialize boilerplate Single page App.

---
+++ King of 20 v2/King of 20 v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  King of 20 v2
+//
+//  Created by Ishmael King on 10/5/20.
+//  Copyright © 2020 Ishmael King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
