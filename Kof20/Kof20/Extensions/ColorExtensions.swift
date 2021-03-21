6d4f761fcb47798a758ba43377bcea7c9c80ea3f
Ishmael King <ishmaelrking@gmail.com>
2021-03-21 16:04:38 -0700

    experiment: Add extension to color

---
+++ Kof20/Kof20/Extensions/ColorExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  ColorExtensions.swift
+//  Kof20
+//
+//  Created by Ishmael King on 10/25/20.
+//
+
+import SwiftUI
+
+extension Color {
+    // #########
+    // Main Menu
+    // #########
+    static var mainBackground: Color {
+        .purple
+    }
+    
+    static var mainHeaderBackground: Color {
+        .purple
+    }
+}
